a99c7e1fe1fcf9c1a0ac53f235c3cb75d26a283f
Rankush <iamrankush@gmail.com>
2023-10-20 00:27:35 +0530

    display div on button click feature added

--- pl_sql/script.js
+++ pl_sql/script.js
@@ -33,3 +33,42 @@ function search() {
         }
     }
 }
+
+
+const sButton = document.querySelectorAll(".item");
+
+//by default hiding <div class="one">
+const display_none_oneClass = document.querySelectorAll(".one");
+for (let i =1; i<display_none_oneClass.length; i++) {
+    display_none_oneClass[i].style.display = "none";
+}
+//end by default hiding <div class="one">
+
+for (let btn of sButton) {
+    btn.addEventListener('click', (e) => {
+        const event = e.target;
+
+        const active = document.querySelector(".active");
+        if (active) {
+            active.classList.remove("active");
+        }
+
+        event.classList.add("active");
+
+
+        const contents = document.querySelectorAll(".one");
+
+        for (let content of contents) {
+            if (content.getAttribute("id") === btn.getAttribute("id")) {
+                content.style.display = "block";
+            }
+            else {
+                content.style.display = "none";
+            }
+        }
+
+
+
+
+    })
+}
